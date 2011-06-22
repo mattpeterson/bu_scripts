69c0e0ab44e3aca4534bb6ace69d88f7fa41065e
Matt Peterson <matt@matthew-peterson.net>
2011-06-22 07:10:34 -0400

    Database updates

--- bin/database.py
+++ bin/database.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 import MySQLdb
+import MySQLdb.cursors
 from collections import defaultdict
 
 class DatabaseConnection(object):
@@ -12,13 +13,13 @@ class DatabaseConnection(object):
         self._connection = MySQLdb.connect(host = host, user = user,
                                            passwd = password, db = db)
     
-    def cursor(self):
+    def cursor(self, cursorclass = MySQLdb.cursors.Cursor):
         """
         Returns the cursor for writing custom queries.
         """
-        return self._connection.cursor()
+        return self._connection.cursor(cursorclass = cursorclass)
     
-    def get_orthogroups(self, org_code = "h37r"):
+    def get_orthogroups(self, org_code = "h37r", key = "gene"):
         """
         Gets the orthogroups for an organism
         
@@ -32,8 +33,42 @@ class DatabaseConnection(object):
         results = cursor.execute("SELECT g.locus, g.orthogroups FROM genes \
 as g WHERE g.org_code LIKE %s", (org_code,))
         
-        for result in results:
-            orthogroups[results[0]].append(results[1])
+        if key == "gene":
+            for result in results:
+                orthogroups[results[0]].append(results[1])
+        elif key == "orthogroup":
+            for result in results:
+                orthogroups[results[1]].append(results[0])
+        else:
+            raise ValueError("Key must be 'gene' or 'orthogroup'.")
         
         return orthogroups
-    
+
+    def get_chip_interactions(self, operons = True, min_score = 12.0, 
+                             lsr2_threshold = 2.0):
+        """Gets the ChIP interactions from the database.  This uses one of
+        the two views Anna set up, either chip_interactions_bd_operons if
+        the `operons` parameter is true, or chip_interactions_bd otherwise.
+        
+        Peaks can be filtered by either lsr2 score (default 2.0 or greater)
+        and also by height over median (default 12.0)
+        """
+        # Creating a new array, as we don't need to store everything
+        results = []
+        
+        cursor = self.cursor(MySQLdb.cursors.DictCursor)
+        
+        if operons:
+            db = "chip_interactions_bd_operons"
+        else:
+            db = "chip_interactions_db"
+        
+        cursor.execute("SELECT * from %s WHERE `height/median` > %s \
+AND (lsr2_ratio = NULL OR lsr2_ratio > %s)" % (db, min_score, lsr2_threshold))
+        
+        for r in cursor.fetchall():
+            results.append((r["from_gene_locus"], r["gene2"]))
+        
+        return results
+        
+        
