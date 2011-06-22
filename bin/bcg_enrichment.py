b96fda01bfac4b7320313f6611f058695f1fbb2a
Matt Peterson <matt@matthew-peterson.net>
2011-06-22 07:10:56 -0400

    BCG enrichment script

---
+++ bin/bcg_enrichment.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env/python
+"""
+A script to read in the results of a BCG ChIP experiment, and calculate 
+enrichment using the MTB COG categories
+"""
+import sys
+from database import DatabaseConnection
+
+def main():
+    # First, let's get BCG orthogroups (gene keyed)
+    db_connection = DatabaseConnection()
+    bcg_to_orthogroup = db_connection.get_orthogroups("bbcg")
+    # Now, the TB orthogroups
+    orthogroup_to_mtb = db_connection.get_orthogroups("h37r", "orthogroups")
+    
+    results_file = sys.argv[1]keyeda    
+
+if __name__ == "__main__":
+    main()
+
