5daea0f81ce53f6c3aad1962565d028a23de2f81
Matthew Peterson <matt@matthew-peterson.net>
2011-06-27 17:55:32 -0400

    Updates

--- bin/clr_network_comparison.py
+++ bin/clr_network_comparison.py
@@ -16,7 +16,7 @@ def get_counts(scores, interactions):
     """
     int_scores = []
     no_int_scores = []
-    
+
     # Split CLR scores into those corresponding to interacting genes
     # and those which do not. Need to look for the reverse of the interaction
     # as well, since the CLR score is symmetric.
@@ -30,9 +30,9 @@ def get_counts(scores, interactions):
                 no_int_scores.append(0.0)
             else:
                 no_int_scores.append(score)
-                
+
     return int_scores, no_int_scores
-    
+
 def get_clr_probability(scores, int_score, no_int_scores):
     """Calculates a hypergeometric pdf to see how well CLR explains the
     network
@@ -41,18 +41,18 @@ def get_clr_probability(scores, int_score, no_int_scores):
     # rather than a straight up cutoff)
     int_hits = len(filter(lambda y: y > 1, int_score))
     no_int_hits =len(filter(lambda y: y > 1, no_int_scores))
-    
+
     # Counts for statistics
     total_hits = int_hits + no_int_hits
     total_genes = len(int_score) + len(no_int_scores)
     num_pulled = len(int_score)
-    
+
     return 1 - hypergeom.cdf(int_hits, total_genes, total_hits, num_pulled)
 
 def main():
     """Main function"""
     connection = DatabaseConnection()
-    
+
     # Retrieve interactions from database
     interactions = connection.get_chip_interactions()
     # from_genes = { i[0] for i in interactions }
@@ -65,26 +65,26 @@ def main():
         int_score, no_score = get_counts(scores, interactions)
         int_scores += int_score
         no_int_scores += no_score
-                
+
         print("%s: %d %f" % (g, len(int_score), 
                             get_clr_probability(scores, int_score, no_score)))
-    
+
     # Now, plot the distributions
     fig = plt.figure()
     ax = fig.add_subplot(211)
     ax.set_xlabel('CLR Z-score')
     ax.set_ylabel('Count')
     ax.set_title('Interacting pairs')
-    
+
     n, bins, patches = ax.hist(int_scores, 25)
     ax = fig.add_subplot(212)
     ax.hist(no_int_scores, bins)
     ax.set_xlabel('CLR Z-score')
     ax.set_ylabel('Count')
     ax.set_title('Non-interacting pairs')
-    
+
     fig.savefig('CLR_scores.png', dpi=150)
-    
+
     plt.show()
 
 if __name__ == "__main__":
--- bin/compare_curation_to_chip.py
+++ bin/compare_curation_to_chip.py
@@ -7,6 +7,9 @@ from database import DatabaseConnection
 import sys
 
 def get_matching_pct(curated_interactions, chip_interactions):
+    """
+    Returns percentage of matching
+    """
     total_curated = len(curated_interactions)
     
     hits = 0
@@ -14,33 +17,41 @@ def get_matching_pct(curated_interactions, chip_interactions):
         if i in chip_interactions:
             hits += 1
     
-    return float(hits) / total_curated
+    return float(hits)
 
 def read_balazi_network(file_handle):
     interactions = set()
     
     for line in file_handle:
         tokens = line.rstrip("\r\n").split("\t")
-        interactions.add( (tokens[2], tokens[5]))
+        if "1" in tokens[4] or "2" in tokens[4]:
+            interactions.add( (tokens[1], tokens[3]))
     
     return interactions
 
 def main():
+    """
+    Main logic
+    """
     connection = DatabaseConnection()
     
     balazi_interactions = read_balazi_network(open(sys.argv[1]))
+
     chip_interactions = connection.get_chip_interactions()
     from_genes = set([ i[0] for i in chip_interactions ])
     
     for g in from_genes:
         curated_interactions = filter(lambda x: x[0] == g, balazi_interactions)
+        chip_interactions_g = filter(lambda x: x[0] == g, chip_interactions)
         
         if len(curated_interactions) == 0:
             print("%s: None Curated" % g)
             continue
+            
+        matches = get_matching_pct(curated_interactions, chip_interactions)
         
-        print("%s: %f" % (g, get_matching_pct(curated_interactions, 
-                                              chip_interactions)))
+        print("%s: %d/%d (%f)" % (g, matches, len(curated_interactions),
+                                              float(matches)/len(curated_interactions)))
         
 
 if __name__ == "__main__":
